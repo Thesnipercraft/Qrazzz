ce5d9b27da70299d902baf8f4015f928ca2d1172
Thesnipercraft <84866344+Thesnipercraft@users.noreply.github.com>
2023-02-22 15:53:08 +0100

    Add files via upload

--- connector.py
+++ connector.py
@@ -1,6 +1,5 @@
 import mysql.connector
 
-
 class UrlQueue:
     def __init__(self, host, user, password, database):
         self.db = mysql.connector.connect(
@@ -13,24 +12,7 @@ class UrlQueue:
         self.create_table_if_not_exists()
 
     def create_table_if_not_exists(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS UrlQueue (id INT AUTO_INCREMENT PRIMARY KEY, url VARCHAR(255))")
-
-    def get_id(self):
-        request = "SELECT id FROM UrlQueue LIMIT 1"
-        self.cursor.execute(request)
-        result = self.cursor.fetchone()
-        if result is None:
-            return None
-        return result[0]
-
-    def get_url(self, id):
-        request = "SELECT url FROM UrlQueue WHERE id = %s"
-        values = (id,)
-        self.cursor.execute(request, values)
-        url = self.cursor.fetchone()
-        if url is None:
-            return None
-        return url[0]
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS UrlQueue (url VARCHAR(2048))")
 
     def add_url(self, url):
         request = "INSERT INTO UrlQueue (url) VALUES (%s)"
@@ -38,13 +20,17 @@ class UrlQueue:
         self.cursor.execute(request, values)
         self.db.commit()
 
-    def delete_url(self, id):
-        request = "DELETE FROM UrlQueue WHERE id = %s"
-        values = (id,)
-        self.cursor.execute(request, values)
+    def get(self):
+        request = "SELECT url FROM UrlQueue LIMIT 1 FOR UPDATE"
+        self.cursor.execute(request)
+        result = self.cursor.fetchone()
+        if result is None:
+            return None
+        url = result[0]
+        delete_request = "DELETE FROM UrlQueue WHERE url = %s"
+        self.cursor.execute(delete_request, (url,))
         self.db.commit()
-
-
+        return url
 
 class DataStore:
 
@@ -78,4 +64,3 @@ class DataStore:
         values = (id,)
         self.cursor.execute(request, values)
         self.db.commit()'''
-
